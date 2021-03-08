1dbc7bca1fdac76ef5c359a9e6e342332a288d14
Michael Bonner <michaelwbonner@gmail.com>
2021-03-08 13:24:05 -0700

    use RegistersUsers on usercontroller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,24 +2,48 @@
 
 namespace App\Http\Controllers\Api;
 
-use JWTAuth;
-use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends ApiController
 {
+	use RegistersUsers;
+
 	public function show()
 	{
 		return Auth::user();
 	}
 
-	public function store(StoreUserRequest $request)
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data)
 	{
-		$user = User::create([
-			'name' => $request->name,
-			'email' => $request->email,
-			'password' => bcrypt($request->password),
+		return Validator::make($data, [
+			'name' => 'required|string|max:255',
+			'email' => 'required|string|email|max:255|unique:users',
+			'password' => 'required|string|min:6|confirmed',
 		]);
-		return JWTAuth::fromUser($user);
+	}
+
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array  $data
+	 * @return \App\Models\User
+	 */
+	protected function create(array $data)
+	{
+		return User::create([
+			'name' => $data['name'],
+			'email' => $data['email'],
+			'password' => Hash::make($data['password']),
+		]);
 	}
 }
